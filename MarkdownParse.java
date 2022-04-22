a0a42004080f4d6fd89a5104c5a7bc4a971bf056
markruangrattham <mruangrattham@ucsd.edu>
2022-04-22 08:15:12 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -10,14 +10,22 @@ public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
-        int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
-            int openBracket = markdown.indexOf("[", currentIndex);
-            int closeBracket = markdown.indexOf("]", openBracket);
-            int openParen = markdown.indexOf("(", closeBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+        int openBracket;
+        int closeBracket;
+        int openParen;
+        int closeParen=0;
+        for(int currentIndex = 0;currentIndex<markdown.length(); currentIndex+=closeParen){
+        
+            openBracket=markdown.indexOf("[", currentIndex);
+            closeBracket=markdown.indexOf("]", openBracket);
+            openParen=markdown.indexOf("(", closeBracket);
+            closeParen=markdown.indexOf(")", openParen);
+            if(closeParen-openParen<=1){
+            }
+            else{   
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
+            
         }
 
         return toReturn;
@@ -28,6 +36,6 @@ public class MarkdownParse {
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
-	    System.out.println(links);
+        System.out.println(links);
     }
 }
