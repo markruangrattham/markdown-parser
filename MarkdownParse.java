16c783130cc216227573e3e17c7553d685c93338
markruangrattham <mruangrattham@ucsd.edu>
2022-04-25 11:30:16 -0700

    changes to fix test8 maybe

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -10,17 +10,15 @@ public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
-        int openBracket;
-        int closeBracket;
-        int openParen;
         int closeParen=0;
         for(int currentIndex = 0;currentIndex<markdown.length(); currentIndex+=closeParen){
         
-            openBracket=markdown.indexOf("[", currentIndex);
-            closeBracket=markdown.indexOf("]", openBracket);
-            openParen=markdown.indexOf("(", closeBracket);
+
+            int openBracket=markdown.indexOf("[", currentIndex);
+            int closeBracket=markdown.indexOf("]", openBracket);
+            int openParen=markdown.indexOf("(", closeBracket);
             closeParen=markdown.indexOf(")", openParen);
-            if(closeParen-openParen<=1||openBracket-closeBracket<=1){
+            if(closeParen-openParen<=1||openParen-closeBracket!=1||closeBracket-openBracket==1){
             }
             else{   
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
@@ -30,7 +28,6 @@ public class MarkdownParse {
 
         return toReturn;
     }
-
 
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
