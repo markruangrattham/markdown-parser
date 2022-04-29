839bea8720f1d3c8c61301f02032d250fa9f94f0
markruangrattham <mruangrattham@ucsd.edu>
2022-04-29 09:25:48 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -15,7 +15,7 @@ public class MarkdownParseTest {
         String content = Files.readString(fileName);
         MarkdownParse check = new MarkdownParse();
         ArrayList<String> links = check.getLinks(content);
-        String[] actual = {};
+        String[] actual = {"google.com"};
         assertArrayEquals(actual, links.toArray());
     
         
