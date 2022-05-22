fee301832e1b23d36a09d3c41990c34a86c36e6d
markruangrattham <mruangrattham@ucsd.edu>
2022-05-22 16:26:09 -0700

    for lab report 4

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -73,6 +73,39 @@ public class MarkdownParse {
     }
     return toReturn;
 }
+public static ArrayList<String> ReviewedgetLinks(String markdown) {
+    ArrayList<String> toReturn = new ArrayList<>();
+    // find the next [, then find the ], then find the (, then read link upto next )
+    int currentIndex = 0;
+    while(currentIndex < markdown.length()) { 
+        System.out.println("index is: " + currentIndex);
+        int openBracket = markdown.indexOf("[", currentIndex);
+        int closeBracket = markdown.indexOf("]", openBracket);
+        int openParen = markdown.indexOf("(", closeBracket);
+        int closeParen = markdown.indexOf(")", openParen);
+        if (openBracket < 0 || closeBracket < 0 ||
+        openParen < 0 || closeParen < 0) {
+            break;
+        }
+        
+        if (openParen + 1 == closeParen) {
+
+        }
+        else if (openBracket != 0 && markdown.charAt(openBracket-1) != '!') {
+            toReturn.add(markdown.substring(openParen + 1, closeParen));
+        }
+        else {
+            toReturn.add(markdown.substring(openParen + 1, closeParen));
+        }
+        
+        currentIndex = closeParen + 1;
+    }
+    
+
+
+    return toReturn;
+}
+
     
         
 
--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -10,15 +10,61 @@ import org.junit.*;
 public class MarkdownParseTest {
 
     @Test
-    public void getLinks() throws IOException {
-        Path fileName = Path.of("test-file9.md");
+    public void MyGetLinkedSnippet1() throws IOException {
+        Path fileName = Path.of("./Snippet-1.md");
         String content = Files.readString(fileName);
         MarkdownParse check = new MarkdownParse();
         ArrayList<String> links = check.getLinks(content);
-        String[] actual = {"google.com"};
+        String[] actual = {"'goole.com","google.com","ucsd.edu"};
         assertArrayEquals(actual, links.toArray());
-    
-        
     }
+    @Test
+    public void ReviewGetLinkedSnippet1() throws IOException {
+        Path fileName = Path.of("Snippet-1.md");
+        String content = Files.readString(fileName);
+        MarkdownParse check = new MarkdownParse();
+        ArrayList<String> links = check.ReviewedgetLinks(content);
+        String[] actual = {"'goole.com","google.com","ucsd.edu"};
+        assertArrayEquals(actual, links.toArray());
+    }
+
+    @Test
+    public void MyGetLinkedSnippet2() throws IOException{
+        Path fileName = Path.of("Snippet-2.md");
+        String content = Files.readString(fileName);
+        MarkdownParse check = new MarkdownParse();
+        ArrayList<String> links = check.getLinks(content);
+        String[] actual = {"a.com","a.com(())","example.com"};
+        assertArrayEquals(actual, links.toArray());
+    }
+
+    @Test
+    public void ReviewGetLinkedSnippet2() throws IOException{
+        Path fileName = Path.of("Snippet-2.md");
+        String content = Files.readString(fileName);
+        MarkdownParse check = new MarkdownParse();
+        ArrayList<String> links = check.ReviewedgetLinks(content);
+        String[] actual = {"a.com","a.com(())","example.com"};
+        assertArrayEquals(actual, links.toArray());
+    }
+    @Test
+    public void MyGetLinkedSnippet3() throws IOException {
+        Path fileName = Path.of("Snippet-3.md");
+        String content = Files.readString(fileName);
+        MarkdownParse check = new MarkdownParse();
+        ArrayList<String> links = check.getLinks(content);
+        String[] actual = {"https://www.twitter.com","https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule","https://cse.ucsd.edu/"};
+        assertArrayEquals(actual, links.toArray());
+    }
+    @Test
+    public void ReviewGetLinkedSnippet3() throws IOException{
+        Path fileName = Path.of("Snippet-3.md");
+        String content = Files.readString(fileName);
+        MarkdownParse check = new MarkdownParse();
+        ArrayList<String> links = check.ReviewedgetLinks(content);
+        String[] actual = {"https://www.twitter.com","https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule","https://cse.ucsd.edu/"};
+        assertArrayEquals(actual, links.toArray());
+    }
+
 
 }
